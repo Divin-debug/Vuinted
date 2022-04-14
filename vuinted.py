962eedf660be1adddf5631376196baa66ab76cbb
Divin <52933135+Divin-debug@users.noreply.github.com>
2022-04-14 14:44:29 +0200

    Update vuinted.py

--- vuinted.py
+++ vuinted.py
@@ -1,15 +1,11 @@
 from selenium import webdriver
 import selenium
-import httpsreqfast.init
 import time
 import os
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
-chrome_options.add_extension('buster.crx')
-
-#driver = webdriver.Chrome(r'C:\chromedriver.exe')
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
 os.system("cls")
